683112aee5cc50c635f204fe46b60d136f8d045a
tooooofu24 <toya24xxx@i.softbank.jp>
2024-05-08 23:34:18 +0900

    add: 天気予報アプリの画面レイアウトを構成する

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,73 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final screenWidth = MediaQuery.of(context).size.width;
+    final halfWidth = screenWidth / 2;
+
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: SizedBox(
+            width: halfWidth,
+            child: const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                AspectRatio(
+                  aspectRatio: 1,
+                  child: Placeholder(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            '** ℃',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            '** ℃',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Center(
+                          child: TextButton(
+                            onPressed: null,
+                            child: Text('Close'),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: TextButton(
+                            onPressed: null,
+                            child: Text('Reload'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
