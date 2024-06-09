0e782152ce644d12d18982c31629ffb7ba00a7ab
tooooofu24 <toya24xxx@i.softbank.jp>
2024-06-09 18:51:51 +0900

    add: riverpodで状態管理

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_training/components/main_result.dart';
-import 'package:flutter_training/state/main_state.dart';
-import 'package:provider/provider.dart';
-import 'package:yumemi_weather/yumemi_weather.dart';
+import 'package:flutter_training/providers/weather_provider.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -28,42 +27,81 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final state = context.watch<MainState>();
-
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.5,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Spacer(),
-                Column(
+    return Consumer(
+      builder: (context, ref, child) {
+        final weather = ref.watch(weatherProvider);
+        return MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: FractionallySizedBox(
+                widthFactor: 0.5,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    MainResult(weatherCondition: state.weatherCondition),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              '** ℃',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelLarge
-                                  ?.copyWith(color: Colors.blue),
-                              textAlign: TextAlign.center,
+                    const Spacer(),
+                    Column(
+                      children: [
+                        switch (weather) {
+                          AsyncData(:final value) => MainResult(
+                              weatherCondition: value.weatherCondition,
                             ),
+                          AsyncError() =>
+                            const Text('Oops, something unexpected happened'),
+                          _ => const CircularProgressIndicator(),
+                        },
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  '** ℃',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelLarge
+                                      ?.copyWith(color: Colors.blue),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  '** ℃',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelLarge
+                                      ?.copyWith(color: Colors.red),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
                           ),
-                          Expanded(
-                            child: Text(
-                              '** ℃',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelLarge
-                                  ?.copyWith(color: Colors.red),
-                              textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 80,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 16, bottom: 16),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text('Close'),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: TextButton(
+                                    onPressed: () =>
+                                        ref.refresh(weatherProvider),
+                                    child: const Text('Reload'),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ],
@@ -71,45 +109,11 @@ class MainScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 80,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 16, bottom: 16),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: const Text('Close'),
-                              ),
-                            ),
-                            Expanded(
-                              child: TextButton(
-                                onPressed: () async {
-                                  try {
-                                    state.fetchWetherCondition();
-                                  } on YumemiWeatherError {
-                                    await _showErrorDialog(context);
-                                  }
-                                },
-                                child: const Text('Reload'),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
